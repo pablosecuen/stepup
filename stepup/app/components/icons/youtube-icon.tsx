368acb9f3f00fb7ede9f6128308faa8056925aaf
pablosecuen <pablosecuen@gmail.com>
2024-02-29 08:52:06 -0300

    abstracting svgs to a separated icons components to reduce footer code

---
+++ stepup/app/components/icons/youtube-icon.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const YouTubeIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-brand-youtube"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M2 8a4 4 0 0 1 4 -4h12a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-12a4 4 0 0 1 -4 -4v-8z" />
+      <path d="M10 9l5 3l-5 3z" />
+    </svg>
+  );
+};
+
+export default YouTubeIcon;
