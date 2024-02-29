f82ac93c04ed3113b760e162e59bee0071642a93
pablosecuen <pablosecuen@gmail.com>
2024-02-29 11:44:24 -0300

    moving product display to store route, and setting up landing page and component

--- stepup/app/(...)/tienda/page.tsx
+++ stepup/app/(...)/tienda/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+import CardContainer from "@/app/components/cardContainer/card-container";
+import Filters from "@/app/components/filters/filters";
+import { useFilter } from "@/app/components/hooks/useFilter";
+import { brands, zapatillasJordan } from "@/app/data";
 import React from "react";
 
 const Tienda = () => {
-  return <div className="max-w-screen-xl mx-auto">Tienda</div>;
+  const { filteredZapatillas, filterByMarca } = useFilter(zapatillasJordan);
+  return (
+    <div className="flex  flex-col items-center pt-4 min-h-screen h-full">
+      <Filters marcas={brands} handleFilterByMarca={filterByMarca} />
+      <CardContainer zapatillasJordan={filteredZapatillas} />
+    </div>
+  );
 };
 
 export default Tienda;
--- stepup/app/page.tsx
+++ stepup/app/page.tsx
@@ -1,15 +1,9 @@
-"use client";
-import CardContainer from "./components/cardContainer/card-container";
-import { brands, zapatillasJordan } from "@/app/data";
-import { useFilter } from "./components/hooks/useFilter";
-import Filters from "./components/filters/filters";
-export default function Home() {
-  const { filteredZapatillas, filterByMarca } = useFilter(zapatillasJordan);
+import Landing from "./components/landing";
 
+export default function Home() {
   return (
-    <main className="flex  flex-col items-center pt-4 min-h-screen h-full">
-      <Filters marcas={brands} handleFilterByMarca={filterByMarca} />
-      <CardContainer zapatillasJordan={filteredZapatillas} />
+    <main className="flex  flex-col items-center  min-h-screen h-full">
+      <Landing />
     </main>
   );
 }
